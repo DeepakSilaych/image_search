6d429cce88e1b915944930f7a968aa1994002c13
Deepak Silaych <deepaksilaych@gmail.com>
2025-12-14 22:49:38 +0530

    threshold based retrival

--- vector_db.py
+++ vector_db.py
@@ -14,19 +14,22 @@ class SearchEngine:
             name="personal_photos",
             metadata={"hnsw:space": "cosine"}
         )
-        self.metadata_generator = ImageEmbedder()
+        self.metadata_generator = None
 
     def add_image(self, image_path):
         # Check if already exists to avoid duplicates
         existing = self.collection.get(where={"path": image_path})
         if existing['ids']:
-            print(f"Skipping {image_path} (Already in DB)")
             return
 
         img = cv2.imread(image_path)
         if img is None:
             print(f"Error reading {image_path}")
             return
+        
+        # so only initialize if adding new images, not during search
+        if self.metadata_generator is None:
+            self.metadata_generator = ImageEmbedder()
 
         # Generate descriptions
         search_text, _ = self.metadata_generator.process(img)
@@ -45,22 +48,31 @@ class SearchEngine:
         )
         print(" -> Saved!")
 
-    def search(self, query_text, n_results=3):
+    def search(self, query_text):
         print(f"\n--- Searching for: '{query_text}' ---")
         
         query_vector = self.embedder.encode(query_text).tolist()
         
         results = self.collection.query(
             query_embeddings=[query_vector],
-            n_results=n_results
         )
-        
-        return results
+
+        final_result = []
+
+        for i in range(len(results["ids"][0])):
+            img_path = results["metadatas"][0][i]["path"]
+            score = results["distances"][0][i]
+
+            if score < 0.8: 
+                final_result.append((img_path, score))
+
+        return final_result
     
 
 if __name__ == "__main__":
     engine = SearchEngine()
     
+
     img_dir = "img/"
     if os.path.exists(img_dir):
 
@@ -74,14 +86,7 @@ if __name__ == "__main__":
             # process image
             engine.add_image(img_path)
 
-        # 2. Search Test
-        results = engine.search("deepak with white tshirt")
-        
-        # Pretty print results
-        if results['ids']:
-            for i in range(len(results['ids'][0])):
-                meta = results['metadatas'][0][i]
-                print(f"File: {results['ids'][0][i]}, Path: {meta['path']}")
-
-    else:
-        print(f"Directory '{img_dir}' not found.")
+    # 2. Search Test
+    results = engine.search("deepak with white tshirt")
+    
+    print(results)
